8687ff8dedbf48a39aedafadde50a89c61e101b7
ilusha2274 <ilya.Shamanin@yandex.ru>
2021-09-13 13:59:22 +0300

    Добавил Http фильтр

---
+++ src/main/java/filter/AuthorizedUserFilter.java
@@ -0,0 +1,23 @@
+package filter;
+
+import repository.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthorizedUserFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        User user = (User) req.getSession().getAttribute("user");
+        if (user == null){
+            res.sendRedirect("/logIn");
+            chain.doFilter(req, res);
+        }
+        chain.doFilter(req, res);
+    }
+}
+
